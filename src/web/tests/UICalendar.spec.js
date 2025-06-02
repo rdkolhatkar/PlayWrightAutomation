904a99b8295e1c21fbe2a05fbe2e01019caa5ac4
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-06-02 13:23:08 +0530

    Calendar assertions for date selection

--- src/web/tests/UICalendar.spec.js
+++ src/web/tests/UICalendar.spec.js
@@ -5,6 +5,7 @@ test.only("Calendar Validation", async ({page})=>{
     const monthNumber = "6";
     const date = "15";
     const year = "2027";
+    const expectedList = [monthNumber, date, year];
 
     await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
     await page.locator(".react-date-picker__inputGroup").click();
@@ -14,4 +15,11 @@ test.only("Calendar Validation", async ({page})=>{
     await page.getByText(year).click();
     await page.locator(".react-calendar__year-view__months__month").nth(Number(monthNumber)-1).click(); //Number(monthNumber) by using this method we can convert string to the number
     await page.locator("//abbr[text()='"+ date +"']").click();
+
+    //To assert and validate the date element we have to use a comman locator as YYYY-MM-DD values are different in nature
+    const inputs = await page.locator(".react-date-picker__inputGroup input");
+    for(let index =0; index <inputs.length; index++){
+        const value = inputs[index].getAttribute("value");
+        expect(value).toEqual(expectedList[index]);
+    } 
 })
