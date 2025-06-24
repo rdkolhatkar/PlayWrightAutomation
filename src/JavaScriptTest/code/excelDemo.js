6842bdbbc929372184303c3199eec9a11a479e1b
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-06-24 12:14:02 +0530

    Renaming the folder utility

--- src/JavaScriptTest/code/excelDemo.js
+++ src/JavaScriptTest/code/excelDemo.js
@@ -2,7 +2,7 @@ const ExcelJs = require('exceljs');
 
 // First Way to run the excel sheet interactions asynchronously 
 const workBook = new ExcelJs.Workbook(); // Creating the object of ExcelJs to use the inbuild methods
-workBook.xlsx.readFile("./src/JavaScriptTestUtils/TestData/TestData.xlsx").then(function () { // Passing the file path of the TestData.xlsx file
+workBook.xlsx.readFile("./src/JavaScriptTest/TestData/TestData.xlsx").then(function () { // Passing the file path of the TestData.xlsx file
 
     const workSheet = workBook.getWorksheet('Sheet1'); // Reading the specific sheet name form excel document
     workSheet.eachRow((row, rowNumber) => { // Iterating the rows using the row function in exceljs
@@ -13,7 +13,7 @@ workBook.xlsx.readFile("./src/JavaScriptTestUtils/TestData/TestData.xlsx").then(
 })
 // Second Way to run the excel sheet interactions asynchronously without .then(function (){})
 async function excelDataTest() {
-    await workBook.xlsx.readFile("./src/JavaScriptTestUtils/TestData/TestData.xlsx");
+    await workBook.xlsx.readFile("./src/JavaScriptTest/TestData/TestData.xlsx");
     const workSheet = workBook.getWorksheet('Sheet1'); // Reading the specific sheet name form excel document
     workSheet.eachRow((row, rowNumber) => { // Iterating the rows using the row function in exceljs
         row.eachCell((cell, colNumber) => { // Iterating the cells using the cell function in exceljs
