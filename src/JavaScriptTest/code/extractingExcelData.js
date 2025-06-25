c450b1b6563827570e9fa265ed8a70c1aefe59f1
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-06-25 13:45:01 +0530

    Extract and Modify excel sheet data

---
+++ src/JavaScriptTest/code/extractingExcelData.js
@@ -0,0 +1,55 @@
+const ExcelJs = require('exceljs');
+
+
+const workBook = new ExcelJs.Workbook();
+workBook.xlsx.readFile("./src/JavaScriptTest/TestData/TestData.xlsx").then(function () {
+    const workSheet = workBook.getWorksheet('Sheet1');
+    workSheet.eachRow((row, rowNumber) => {
+        row.eachCell((cell, colNumber) => {
+            // Now we have to extract a specific value from the excel sheet using row number and coloumn number co-ordinates
+            if (cell.value === "Apple") {
+                console.log("Row Number of the given value is " + rowNumber);
+                console.log("Coloumn Number of the given value is " + colNumber);
+            }
+        });
+    });
+});
+
+// Now we have to replace the cell value 'Apple' with another value called 'Iphone' in the ModifyData.xlsx sheet
+async function modifyExcelData() {
+    await workBook.xlsx.readFile("./src/JavaScriptTest/TestData/ModifyData.xlsx")
+    const workSheet = workBook.getWorksheet('Sheet1');
+    workSheet.eachRow((row, rowNumber) => {
+        row.eachCell((cell, colNumber) => {
+            // Now we have to extract a specific value from the excel sheet using row number and coloumn number co-ordinates
+            if (cell.value === "Apple") {
+                console.log("Row Number of the given value is " + rowNumber);
+                console.log("Coloumn Number of the given value is " + colNumber);
+            }
+        });
+    });
+    const cell = workSheet.getCell(3, 2); //.getCell(row, coloumn) this method is used to get the cell co-ordinates for updating the cell value
+    cell.value = "Iphone"; // cell.value method is used to modify the existing cel value with new cell value
+    await workBook.xlsx.writeFile("./src/JavaScriptTest/TestData/ModifyData.xlsx") //workBook.xlsx.writeFile() method is used to save the modified excel workbook
+}
+modifyExcelData();
+
+// Now we have to extract the colNumber & rowNumber dynamically and place that value inside the method "workSheet.getCell(row, coloumn)"
+async function modifyExcelDataTest() {
+    let output = {row:1,column:1}; // creating JavaScript object
+    await workBook.xlsx.readFile("./src/JavaScriptTest/TestData/ModifyData.xlsx")
+    const workSheet = workBook.getWorksheet('Sheet1');
+    workSheet.eachRow((row, rowNumber) => {
+        row.eachCell((cell, colNumber) => {            
+            if (cell.value === "Iphone") {
+                output.row = rowNumber; // Assigning the global object to the rowNumber
+                output.column = colNumber; // Assigning the global object to the colNumber
+            }
+        });
+    });
+    const cell = workSheet.getCell(output.row, output.column); //.getCell(row, coloumn) this method is used to get the cell co-ordinates for updating the cell value
+    cell.value = "Apple"; // cell.value method is used to modify the existing cel value with new cell value
+    await workBook.xlsx.writeFile("./src/JavaScriptTest/TestData/ModifyData.xlsx") //workBook.xlsx.writeFile() method is used to save the modified excel workbook
+}
+modifyExcelDataTest();
+
