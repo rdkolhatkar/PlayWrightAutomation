2c354cd4fb620745009ff11f0dfa181b7b9ef83e
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-06-26 14:43:14 +0530

    Dynamically Updating the Price column

--- src/JavaScriptTest/code/extractingExcelData.js
+++ src/JavaScriptTest/code/extractingExcelData.js
@@ -36,11 +36,11 @@ modifyExcelData();
 
 // Now we have to extract the colNumber & rowNumber dynamically and place that value inside the method "workSheet.getCell(row, coloumn)"
 async function modifyExcelDataTest() {
-    let output = {row:1,column:1}; // creating JavaScript object
+    let output = { row: 1, column: 1 }; // creating JavaScript object
     await workBook.xlsx.readFile("./src/JavaScriptTest/TestData/ModifyData.xlsx")
     const workSheet = workBook.getWorksheet('Sheet1');
     workSheet.eachRow((row, rowNumber) => {
-        row.eachCell((cell, colNumber) => {            
+        row.eachCell((cell, colNumber) => {
             if (cell.value === "Iphone") {
                 output.row = rowNumber; // Assigning the global object to the rowNumber
                 output.column = colNumber; // Assigning the global object to the colNumber
@@ -52,4 +52,55 @@ async function modifyExcelDataTest() {
     await workBook.xlsx.writeFile("./src/JavaScriptTest/TestData/ModifyData.xlsx") //workBook.xlsx.writeFile() method is used to save the modified excel workbook
 }
 modifyExcelDataTest();
+
+//Creating the Separate function for reading and writing the Excel sheet data.
+// Creating a Generic method or utility for reading the cell value
+async function readExcelData(workSheet, searchTextData) {
+    let output = { row: 1, column: 1 };
+    workSheet.eachRow((row, rowNumber) => {
+        row.eachCell((cell, colNumber) => {
+            if (cell.value === searchTextData) {
+                output.row = rowNumber;
+                output.column = colNumber;
+            }
+        });
+    });
+    return output;
+};
+
+async function writeExcel(searchTextData, replaceTextData, filePath) {
+
+    await workBook.xlsx.readFile(filePath);
+    const workSheet = workBook.getWorksheet('Sheet1');
+    const output = await readExcelData(workSheet, searchTextData); // Creating a separate function for reading the excel data
+    const cell = workSheet.getCell(output.row, output.column);
+    cell.value = replaceTextData;
+    await workBook.xlsx.writeFile(filePath);
+};
+
+writeExcel("Banana", "Orange", "./src/JavaScriptTest/TestData/ModifyData.xlsx"); //writeExcel("searchTextData","replaceTextData","filePath") We are passing these arguments dynamically
+
+// Update Mango price to 350
+async function readExcelTestData(workSheet, searchTextData) {
+    let output = { row: 1, column: 1 };
+    workSheet.eachRow((row, rowNumber) => {
+        row.eachCell((cell, colNumber) => {
+            if (cell.value === searchTextData) {
+                output.row = rowNumber;
+                output.column = colNumber;
+            }
+        });
+    });
+    return output;
+};
+async function writeExcelTest(searchTextData, replaceTextData, change, filePath) {
+
+    await workBook.xlsx.readFile(filePath);
+    const workSheet = workBook.getWorksheet('Sheet1');
+    const output = await readExcelData(workSheet, searchTextData);
+    const cell = workSheet.getCell(output.row, output.column + change.colChange);// Adding the y co-ordinate to the cell column co-ordinate to reah the price column co-ordinate
+    cell.value = replaceTextData;
+    await workBook.xlsx.writeFile(filePath);
+};
+writeExcelTest("Mango", 350, { rowChange: 0, colChange: 2 }, "./src/JavaScriptTest/TestData/ModifyData.xlsx");
 
