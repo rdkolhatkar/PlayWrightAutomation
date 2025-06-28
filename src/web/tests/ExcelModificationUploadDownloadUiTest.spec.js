c5c2fdf52f1b0873bd71af50c9b831cdeeec2c21
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-06-28 10:24:32 +0530

    Excel validation and Assertions

--- src/web/tests/ExcelModificationUploadDownloadUiTest.spec.js
+++ src/web/tests/ExcelModificationUploadDownloadUiTest.spec.js
@@ -5,14 +5,20 @@ const { ExcelFileUtils } = require('../utils/ExcelFileUtils');
 // After uploading the modified excel document we have to check if our UI table is displaying the modified excel values in the browser table or not.
 test('Upload & Download Excel Validation', async ({ page }) => {
 
+    const textSearch = 'Mango';
+    const updateValue = 350;
     await page.goto("https://rahulshettyacademy.com/upload-download-test/index.html");
     const downloadPromise = page.waitForEvent('download');
     await page.getByRole('button', { name: 'Download' }).click();
     await downloadPromise;
-    ExcelFileUtils.writeExcelTest("Sheet1","Mango", 350, { rowChange: 0, colChange: 2 }, "./src/web/data/ModifyTest.xlsx");
+    ExcelFileUtils.writeExcelTest("Sheet1", textSearch, 350, { rowChange: 0, colChange: 2 }, "./src/web/data/ModifyTest.xlsx");
     await page.locator("#fileinput").click(); // After clicking on the upload button a new file explorar will open and from there we have to upload the excel sheet. But the file explora is not part of the Web.
     // File explorar is part of local system of our computer. To upload the file we will use the below code
     await page.locator("#fileinput").setInputFiles("./src/web/data/ModifyTest.xlsx");
     // .setInputFiles("./filePath") this is inbuild playwright method for uploading the file. But to use this method we have to make sure that HTML tag should contain {<input type='file' accept=.xlsx,.xlx>::</input>} this syntax
+    // Now we have to validate after uploading the updated excel sheet. The UI table got updated with the new values or not.
+    const textLocator = page.getByText(textSearch);
+    const desiredRow = await page.getByRole("row").filter({ has: textLocator });
+    await expect(desiredRow.locator("#cell-4-undefined")).toContainText(updateValue);
 
 })
