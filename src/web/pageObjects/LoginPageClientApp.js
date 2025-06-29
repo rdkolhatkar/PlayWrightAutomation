7fe1c703927a3c0a1099fcd9751579bbeff7f409
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-06-29 16:48:38 +0530

    Implementing Page Object Model

---
+++ src/web/pageObjects/LoginPageClientApp.js
@@ -0,0 +1,18 @@
+class LoginPageClientApp {
+    constructor(page) {
+        this.page = page;
+        this.signInButton = page.locator("[value='Login']");
+        this.userName = page.locator("#userEmail");
+        this.loginPassword = page.locator("#userPassword");
+    }
+    async navigateToClientApp(){
+        await this.page.navigateToClientApp("https://rahulshettyacademy.com/client");
+    };
+    // This method is common for all the test cases for client app
+    async validLogin(username, password) {
+        await this.userName.fill(username);
+        await this.loginPassword.fill(password);
+        await this.signInButton.click();
+    }
+};
+module.exports = { LoginPageClientApp };
