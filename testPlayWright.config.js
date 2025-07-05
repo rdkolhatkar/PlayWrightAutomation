6eb68e160541cf83b71a145e5cbd297835198cdb
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-07-05 13:38:51 +0530

    creating custom configuration files

---
+++ testPlayWright.config.js
@@ -0,0 +1,104 @@
+// @ts-check
+import { chromium, defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+const config = ({
+  testDir: './src',
+  /*Adding Timeouts */
+  timeout: 40 * 1000,
+  /*Adding timeouts for Assertions */
+  expect: {
+    timeout: 4 * 1000,
+  },
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  // This syntax is used for defining multiple properties related to the project
+  projects: [
+    {
+      name: 'safari browser test',
+      use: {
+        browserName: 'webkit', // setting the browswe name
+        headless: true,
+        screenshot: 'off',
+        trace: 'retain-on-failure',
+      }
+    },
+    {
+      name: 'chrome browser test',
+      use: {
+        browserName: 'chromium', // setting the browswe name
+        headless: false,
+        screenshot: 'on',
+        trace: 'on',
+      }
+    },
+
+  ],
+
+  /* Configure projects for major browsers */
+
+  // projects: [c
+  //   {
+  //     name: 'chromium',
+  //     use: { ...devices['Desktop Chrome'] },
+  //   },
+
+  //   {
+  //     name: 'firefox',
+  //     use: { ...devices['Desktop Firefox'] },
+  //   },
+
+  //   {
+  //     name: 'webkit',
+  //     use: { ...devices['Desktop Safari'] },
+  //   },
+
+  /* Test against mobile viewports. */
+  // {
+  //   name: 'Mobile Chrome',
+  //   use: { ...devices['Pixel 5'] },
+  // },
+  // {
+  //   name: 'Mobile Safari',
+  //   use: { ...devices['iPhone 12'] },
+  // },
+
+  /* Test against branded browsers. */
+  // {
+  //   name: 'Microsoft Edge',
+  //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+  // },
+  // {
+  //   name: 'Google Chrome',
+  //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+  // },
+  // ],
+
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
+module.exports = config
+
