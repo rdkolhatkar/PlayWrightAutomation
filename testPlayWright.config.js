f791ddf96050c43951ceed47d5be4458121e5738
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-07-07 19:45:00 +0530

    Adding retry logic

--- testPlayWright.config.js
+++ testPlayWright.config.js
@@ -2,14 +2,6 @@
 import { chromium, defineConfig, devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * @see https://playwright.dev/docs/test-configuration
  */
 const config = ({
@@ -25,7 +17,8 @@ const config = ({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  // Retry Flaky Tests, We are using the below syntax to execute retry logic for the test execution
+  retries: 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
