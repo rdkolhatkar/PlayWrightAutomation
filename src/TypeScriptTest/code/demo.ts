a59843bcb9688d7d37df11b1d99425a63d8a97ac
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-07-13 17:36:17 +0530

    Installed TypeScript packages

---
+++ src/TypeScriptTest/code/demo.ts
@@ -0,0 +1,16 @@
+let message : string = "Hello World"; // Type Script syntax for declaring string variable
+console.log(message);
+let age : number = 20; // Type Script syntax for declaring integer variable
+console.log(age);
+let isActive : boolean = false; // Type Script syntax for declaring boolean variable
+console.log(isActive);
+let numArray : number[] = [1, 2, 3]; // Type Script syntax for declaring Array
+console.log(numArray);
+let data : any = "This could be anything like String, Integer, array etc.";
+console.log(data);
+data = 42; // As we have given 'any' as data type value to the variable 'data', we can use data varable for anything.
+console.log(data);
+data = [1,2,3];
+console.log(data);
+data = true; 
+console.log(data);
