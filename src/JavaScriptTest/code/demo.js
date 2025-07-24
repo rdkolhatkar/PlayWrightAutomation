816143401c711650e0a81dc41242724f65886119
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-07-24 17:30:03 +0530

    Core JavaScript

---
+++ src/JavaScriptTest/code/demo.js
@@ -0,0 +1,25 @@
+// Printing Hello World in JavaScript
+console.log("Hello World"); // To run the javaScript file we use command "node demo.js"
+// node src/JavaScriptTest/code/demo.js
+// Declaring the variables in JavaScript
+// We use the keywords like var, let and const keywords to declare a variable in JavaScript
+// We cannot redeclare the variable with "let" keyword but it is possible with "var" 
+let a = 4;
+let b = "String";
+let c = true;
+let d = 3.1815;
+// To understand the data type of any variable we use typeof() method in JavaScript
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+// Performing operations on variables
+var num1 = 10;
+var num2 = 20;
+var num1 = 30;
+var num3 = num1 + num2;
+console.log(num3);
+// Negation of boolean values
+console.log(!c) // true will be converted to the false
+// We can reassign new values to "let" & "var" keywords but we cannot reassign new values to the "const" keyword
+
